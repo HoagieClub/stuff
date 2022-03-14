e6baa35815c91f4051953e8e385bc3e8f2f6276e
Sean Wang <seansmoove27@gmail.com>
2022-03-13 23:59:40 -0400

    Page for Individual Bulletin

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { majorScale, Pane } from 'evergreen-ui';
+import { Pane, majorScale, ArrowsHorizontalIcon, Button, ChatIcon} from 'evergreen-ui'
+import Link from 'next/link'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import MenuCard from '../../components/MenuCard';
 import router from "next/router";
@@ -23,6 +24,66 @@ export default withPageAuthRequired(() => {
     <Pane display="flex" flexWrap="wrap" justifyContent="center" 
     marginY={majorScale(4)}
     paddingX={majorScale(3)}>
+       <Pane 
+            display="flex"
+            borderRadius={8}
+            elevation={1} 
+            background="white" 
+            marginX={20} 
+            maxWidth="800px" 
+            height="400px"
+            width="100%"
+            paddingX="10px"
+            paddingTop={majorScale(5)}
+            paddingBottom={majorScale(7)}>
+              <div style={{width:"100%", display:"flex", justifyContent:"space-between"}}> 
+            
+            <div style={{marginLeft:40}}>
+                <div style={{marginBottom:30, fontSize:30}}><b>Tiger Plushie</b></div>
+              <div style={{fontSize:15}}>
+              <div style={{marginBottom:20}}><b>Description:</b> Soft and like new. Is a hand sized tiger plushie.</div>
+              <div style={{marginBottom:20}}><b>Type of Trade:</b> Selling</div>
+              <div style={{marginBottom:20}}><b>Delivery:</b> Pickup</div>
+              </div>
+
+              <div style={{display:"flex", justifyContent:"space-between", marginTop:50}}><Button
+            height={45}
+            width={majorScale(25)}
+            appearance="default"
+            marginTop={20}
+            iconBefore={ArrowsHorizontalIcon}
+            backgroundColor="#6E62B6"
+            color="white"
+          >
+              <div><b>Request to Buy</b></div>
+          </Button>
+
+          <Button
+            height={45}
+            width={majorScale(25)}
+            appearance="default"
+            marginTop={20}
+            iconBefore={ChatIcon}
+            color="#6E62B6"
+            backgroundColor="white"
+            borderColor="#6E62B6"
+          >
+              <div><b>Ask a Question</b></div>
+          </Button>
+
+          </div>
+
+            </div>
+
+            <div style={{marginRight:40, display:"flex flex-col", justifyContent:"flex-end", fontSize:15}}>
+              <div>5 days left</div>
+              <div style={{color:"grey"}}>2 standing offers</div>
+            </div>
+            </div>
+           
+         
+        </Pane>
+      
     </Pane>
     </>
   );
