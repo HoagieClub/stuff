c7688d50f5bb449d7a8ad3fb95d22ab065f40cea
AnanyaGrover1 <ananyag@princeton.edu>
2022-03-26 14:51:53 -0400

    eslint style fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,16 @@ import '../lib/hoagie-ui/theme.css';
 
 function Content({ Component, pageProps }) {
     const user = useUser();
-    const tabs = [{href: '/home', title: "Home"}, {href: '/about', title: "About"}, {href: '/logout', title: "Logout"}] 
+    const tabs = [
+        { href: '/home', title: 'Home' },
+        { href: '/about', title: 'About' },
+        { href: '/logout', title: 'Logout' },
+    ]
 
     return (
         <Theme palette="gray">
             <Layout>
-                <Nav name="stuff" tabs= {tabs} user={user} />
+                <Nav name="stuff" tabs={tabs} user={user} />
                 <Component {...pageProps} />
                 <Footer />
             </Layout>
