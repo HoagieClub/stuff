f5b47388831a530d8a61e6395d2068ce3a19c888
Ananya Grover <71813212+AnanyaGrover1@users.noreply.github.com>
2022-03-27 14:18:52 -0400

    feat: added initial tabs (#9)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,16 @@ import '../lib/hoagie-ui/theme.css';
 
 function Content({ Component, pageProps }) {
     const user = useUser();
+    const tabs = [
+        { href: '/home', title: 'Home' },
+        { href: '/about', title: 'About' },
+        { href: '/logout', title: 'Logout' },
+    ]
 
     return (
         <Theme palette="gray">
             <Layout>
-                <Nav name="stuff" user={user} />
+                <Nav name="stuff" tabs={tabs} user={user} />
                 <Component {...pageProps} />
                 <Footer />
             </Layout>
