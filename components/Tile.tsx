500320406f27c8304cedb4e2f0c8308b49bf3a06
Edmund <edmund666@princeton.edu>
2022-03-27 14:27:35 -0400

    finalized tiles

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,6 +1,26 @@
-import { Pane, Badge } from 'evergreen-ui';
+import { Pane, Badge, Button, InfoSignIcon, ArrowTopRightIcon } from 'evergreen-ui';
 
 export default function Tile({ tile }) {
+    // assign colors to badges
+    let col = '';
+    if (tile.type === 'Student Sale') {
+        col = 'red';
+    } else if (tile.type === 'Lost & Found') {
+        col = 'green';
+    } else if (tile.type === 'Bulletin') {
+        col = 'purple';
+    } else {
+        col = 'yellow';
+    }
+
+    // Buttons
+    let b1text = '';
+    if (col === 'red') {
+        b1text = 'Open Slides';
+    } else {
+        b1text = 'Contact';
+    }
+
     return (
         <Pane>
             <Pane
@@ -14,9 +34,10 @@ export default function Tile({ tile }) {
                 position="relative"
                 flexWrap="wrap"
             >
-                <Badge marginLeft={0} fontSize={15}>{tile.type}</Badge>
+                <Badge color={col} marginLeft={12} fontSize={15}>{tile.type}</Badge>
                 <Pane
                     marginTop={20}
+                    marginLeft={12}
                     display="flex"
                     flexDirection="column"
                     alignItems="flex-start"
@@ -24,16 +45,27 @@ export default function Tile({ tile }) {
                     <span style={{ fontSize: '20px' }}><b>{tile.title}</b></span>
                     <span style={{ fontSize: '15px' }}><b>${tile.amt}</b></span>
                     <img id="listingImage" src={tile.thumbnail} alt={tile.title} />
+                    <Pane paddingTop={15} fontSize='15px'>
+                        {tile.description}
+                    </Pane>
                     <Pane paddingTop={10}>
                         {tile.tags.map(
                             (tag: string) => (
-                                <Badge marginLeft={0} fontSize={15}>
+                                <Badge color={col} fontSize={15}>
                                     {tag}
                                 </Badge>
                             ),
                         )}
                     </Pane>
                 </Pane>
+                <Button marginLeft={12} marginTop={15} size="medium"
+                    iconBefore={ArrowTopRightIcon}>
+                    {b1text}
+                </Button>
+                <Button marginLeft={10} marginTop={15} marginRight={12}
+                    size="medium" iconBefore={InfoSignIcon}>
+                    Read More
+                </Button>
             </Pane>
         </Pane>
     );
