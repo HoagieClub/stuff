38875692777bc1750cd16001be29703de9543545
gagik <gagik@princeton.edu>
2022-05-01 17:33:09 -0400

    fix: Update code remover.

--- pages/all/index.tsx
+++ pages/all/index.tsx
@@ -1,5 +1,22 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import PostPage from '../../components/PostPage';
 // import { PostData } from '../types';
 
-export default withPageAuthRequired(() => <PostPage pageNumber={1} />);
+export default withPageAuthRequired(() => {
+    const router = useRouter()
+    useEffect(() => {
+        // eslint-disable-next-line no-restricted-globals
+        const queryParams = new URLSearchParams(location.search)
+
+        if (queryParams.has('code')) {
+            queryParams.delete('code')
+            queryParams.delete('state')
+            // TODO: add support for other params to persist using
+            // queryParam.toString() or remove the queryParams method
+            router.replace('/all', undefined, { shallow: true })
+        }
+    }, [])
+    return <PostPage pageNumber={1} />
+});
