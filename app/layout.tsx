57076fb8a582ad5c7717f33b18ee25d110833413
Jenny Fan <jennyfan2022@yahoo.com>
2025-09-17 01:52:14 -0400

    Add hoagie easter egg

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { Metadata } from 'next';
 
 import Content from '@/app/Content';
+import { hoagie } from '@/app/hoagie';
 
 export const metadata: Metadata = {
     title: 'Stuff by Hoagie',
@@ -14,6 +15,13 @@ export const metadata: Metadata = {
 export default function App({ children }: { children: ReactNode }) {
     return (
         <html lang='en'>
+            <head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(${hoagie.toString()})();`,
+                    }}
+                />
+            </head>
             <UserProvider>
                 <body>
                     <Content>{children}</Content>
