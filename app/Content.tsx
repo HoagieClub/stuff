27e15ce3d658027efe095e1b486411e81d3b807d
prishaakapasi <96963490+prishaakapasi@users.noreply.github.com>
2025-11-20 15:32:48 -0500

    Dev-273 Change Theme Colors (#33)
    * added theme changes
    
    * theme colors
    
    * changedrainbow
    
    * Prettier fix
    
    ---------
    
    Co-authored-by: Prishaa Kapasi <prishaakapasi@dynamic-oit-ip4-wifirestricted03-10-49-142-44.princeton.edu>
    Co-authored-by: Prishaa Kapasi <prishaakapasi@dynamic-oit-ip4-wifirestricted03-10-48-255-237.princeton.edu>
    Co-authored-by: Jenny Fan <40123244+jfmath04@users.noreply.github.com>

--- app/Content.tsx
+++ app/Content.tsx
@@ -3,7 +3,6 @@
 import React, { ReactNode } from 'react';
 
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { Pane, Text, Paragraph } from 'evergreen-ui';
 import { Metadata } from 'next';
 
 import Layout from '@/lib/hoagie-ui/Layout';
@@ -16,59 +15,6 @@ import '@/app/stuff.css';
 export const metadata: Metadata = {
     title: 'Stuff by Hoagie',
 };
-
-const RainbowLogo = () => (
-    <Pane whiteSpace='nowrap'>
-        <Text
-            is='h2'
-            display='inline-block'
-            className='hoagie logo'
-            color='grey900'
-        >
-            hoagie
-        </Text>
-        <Paragraph
-            is='h2'
-            display='inline-block'
-            color='blue500'
-            className='hoagie logo rainbow-text'
-        >
-            stuff
-        </Paragraph>
-    </Pane>
-);
-
-const RainbowHeader = () => (
-    <Pane
-        width='100%'
-        height={20}
-        display='flex'
-        flexDirection='row'
-        style={{
-            backgroundImage: `linear-gradient(
-                90deg,
-                hsl(0deg 73% 75%) 0%,
-                hsl(12deg 94% 76%) 19%,
-                hsl(22deg 100% 75%) 27%,
-                hsl(33deg 100% 74%) 34%,
-                hsl(44deg 82% 72%) 41%,
-                hsl(69deg 51% 70%) 47%,
-                hsl(107deg 44% 75%) 53%,
-                hsl(149deg 45% 73%) 59%,
-                hsl(175deg 52% 64%) 66%,
-                hsl(190deg 77% 57%) 73%,
-                hsl(201deg 97% 62%) 81%,
-                hsl(225deg 100% 76%) 100%
-              )`,
-        }}
-    >
-        {/* <Pane width="20%" height={20} background="red500" />
-            <Pane width="20%" height={20} background="yellow300" />
-            <Pane width="20%" height={20} background="green300" />
-            <Pane width="20%" height={20} background="teal300" />
-            <Pane width="20%" height={20} background="rblue300" /> */}
-    </Pane>
-);
 
 export default function Content({
     children,
@@ -86,13 +32,7 @@ export default function Content({
     return (
         <Theme palette='gray'>
             <Layout>
-                <Nav
-                    name='stuff'
-                    tabs={tabs}
-                    user={user?.user}
-                    LogoComponent={RainbowLogo}
-                    HeaderComponent={RainbowHeader}
-                />
+                <Nav name='stuff' tabs={tabs} user={user?.user} />
                 {children}
             </Layout>
         </Theme>
