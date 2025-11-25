a3bf988ac477db23b2ad0df94f338e28ba756674
Anna Lau <lauanna916@gmail.com>
2025-11-25 16:20:28 -0500

    fixed eslint errors

--- app/bulletins/[pid]/page.tsx
+++ app/bulletins/[pid]/page.tsx
@@ -4,10 +4,6 @@ import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
 import PostPage from '@/components/PostPage';
 
-type Params = {
-    params: { pid: string };
-};
-
 export default withPageAuthRequired(() => {
     return <PostPage category='bulletin' />;
 });
