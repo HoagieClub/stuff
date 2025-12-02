e03ac9d1df0d701e0b5cc5a62fa5a587729bf034
anna-1au <lauanna916@gmail.com>
2025-12-02 15:38:21 -0500

    DEV-229 Infinite Scroll (#40)

--- app/lost/[pid]/page.tsx
+++ app/lost/[pid]/page.tsx
@@ -4,12 +4,6 @@ import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
 import PostPage from '@/components/PostPage';
 
-type Params = {
-    params: { pid: string };
-};
-
-export default withPageAuthRequired(({ params }: Params) => {
-    const { pid } = params;
-    const pageNumber = pid ? parseInt(pid, 10) : 1;
-    return <PostPage pageNumber={pageNumber} category='lost' />;
+export default withPageAuthRequired(() => {
+    return <PostPage category='lost' />;
 });
